56f9a2f59130878a0854b67b6cf7138626295106
merjemole <89482236+merjemole@users.noreply.github.com>
2024-12-31 01:18:07 +0100

    Delete carousel.js

--- carousel.js
+++
@@ -1,9 +0,0 @@
-const carousel = document.querySelector('.carousel');
-
-carousel.addEventListener('mouseover', () => {
-    carousel.style.animationPlayState = 'paused';
-});
-
-carousel.addEventListener('mouseout', () => {
-    carousel.style.animationPlayState = 'running';
-});
