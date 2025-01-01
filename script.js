b811c986de9cc389c3cf14771a4ecbddb337ef41
merjemole <89482236+merjemole@users.noreply.github.com>
2025-01-02 00:00:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,3 @@ navLinks.forEach(link => {
         link.style.color = '#333';
     });
 });
-
-// Ny kod för att lägga till "blurrad bakgrund" vid scroll
-document.addEventListener("scroll", function () {
-    const nav = document.querySelector(".collaboration-page .floating-nav");
-    if (window.scrollY > 10) { // Lägg till klass när man skrollar mer än 10px
-        nav.classList.add("scrolled");
-    } else {
-        nav.classList.remove("scrolled");
-    }
-});
